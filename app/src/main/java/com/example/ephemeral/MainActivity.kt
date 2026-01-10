c9f4a7a9c95116a436c4697601533f970ffc62ad
Nicolas Balao <nicolasbalao.pro@gmail.com>
2026-01-10 17:27:43 +0100

    feat(chore): Setup webview with a search bar

--- app/src/main/java/com/example/ephemeral/MainActivity.kt
+++ app/src/main/java/com/example/ephemeral/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.ephemeral
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private lateinit var urlBar: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +27,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        webView = findViewById<WebView>(R.id.webview)
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                urlBar.setText(url)
+            }
+
+        }
+
+        webView.loadUrl("https://duckduckgo.com/")
+        webView.settings.javaScriptEnabled = true;
+
+
+        urlBar = findViewById<EditText>(R.id.urlBar)
+
+        urlBar.setOnEditorActionListener { v, actionId, _ ->
+
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                val input = urlBar.text.toString()
+                webView.loadUrl(
+                    "https://duckduckgo.com/?q=${
+                        URLEncoder.encode(
+                            input,
+                            "UTF-8"
+                        )
+                    }"
+                )
+                true
+            } else false
+        }
+
+
     }
 }
